f97a7ca526827e461b13b2a693aacbf08a95708a
Makubu <58472817+Makubu@users.noreply.github.com>
2021-02-18 16:05:00 +0100

    up

--- visuals/genreByRegionWithAge/mapApp.js
+++ visuals/genreByRegionWithAge/mapApp.js
@@ -68,7 +68,10 @@ function hideMapInfo(event) {
 
 
 //Import data
-dataP = d3.csv("small_data.csv"); // dataP pour data Promise
+console.log(window.location.pathname);
+console.log(window.location.href);
+
+dataP = d3.csv("/small_data.csv"); // dataP pour data Promise
 
 //To keep track of the listeners
 let currentListners = {};
